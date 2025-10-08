260aaee757e7ca51ee4e0bcb7bc4d02b31aff233
montelzek <patrykmatras378@gmail.com>
2025-10-08 19:33:30 +0200

    feat(db): Add roles and user_roles tables

---
+++ backend/src/main/resources/db/migration/V2__create_roles_and_user_roles.sql
@@ -0,0 +1,18 @@
+CREATE TABLE roles
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+);
+
+INSERT INTO roles (name) VALUES
+('RESIDENT'),
+('ADMIN'),
+('TECHNICIAN'),
+('RECEPTIONIST');
+
+CREATE TABLE user_roles
+(
+    user_id INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    role_id INTEGER NOT NULL REFERENCES roles (id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, role_id)
+);
