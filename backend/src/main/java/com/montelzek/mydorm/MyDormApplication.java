ad2d7ac102c38108763809cfbc9418e11718e5af
montelzek <patrykmatras378@gmail.com>
2025-10-08 16:22:55 +0200

    Initial commit: backend + frontend setup

---
+++ backend/src/main/java/com/montelzek/mydorm/MyDormApplication.java
@@ -0,0 +1,13 @@
+package com.montelzek.mydorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDormApplication.class, args);
+	}
+
+}
