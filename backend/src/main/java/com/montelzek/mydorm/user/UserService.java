3cd7bc0c94ed25fe21fe7213871c875aba1dee8a
montelzek <patrykmatras378@gmail.com>
2025-10-10 20:09:38 +0200

    feat: Add initial UserService class

---
+++ backend/src/main/java/com/montelzek/mydorm/user/UserService.java
@@ -0,0 +1,12 @@
+package com.montelzek.mydorm.user;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+}
