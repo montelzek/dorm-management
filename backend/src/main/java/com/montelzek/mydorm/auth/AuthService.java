9397a670ae1196546781b0f4656659d418954cbc
montelzek <patrykmatras378@gmail.com>
2025-10-10 20:14:45 +0200

    feat(service): Implement AuthService for login and registration
    Introduces the `AuthService` to encapsulate user authentication logic. This service provides two main methods:
    
    - **`login`**: Authenticates the user, generates a JWT, sets the security context, and returns the token and user details.
    - **`register`**: Creates a new user with an encoded password, assigns the default `ROLE_RESIDENT`, and checks for existing email conflicts.

---
+++ backend/src/main/java/com/montelzek/mydorm/auth/AuthService.java
@@ -0,0 +1,68 @@
+package com.montelzek.mydorm.auth;
+
+import com.montelzek.mydorm.auth.payload.JwtResponse;
+import com.montelzek.mydorm.auth.payload.LoginInput;
+import com.montelzek.mydorm.auth.payload.RegisterInput;
+import com.montelzek.mydorm.security.UserDetailsImpl;
+import com.montelzek.mydorm.security.jwt.JwtUtils;
+import com.montelzek.mydorm.user.ERole;
+import com.montelzek.mydorm.user.User;
+import com.montelzek.mydorm.user.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtils jwtUtils;
+
+    public JwtResponse login(LoginInput loginInput) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginInput.email(), loginInput.password())
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+
+        return new JwtResponse(jwt, userDetails.getId(), userDetails.getEmail(), userDetails.getFirstName(), roles);
+    }
+
+    public void register(RegisterInput registerInput) {
+
+        if (userRepository.existsByEmail(registerInput.email())) {
+            throw new IllegalArgumentException("Error: Email is already taken!");
+        }
+
+        User user = new User();
+        user.setFirstName(registerInput.firstName());
+        user.setLastName(registerInput.lastName());
+        user.setEmail(registerInput.email());
+        user.setPassword(passwordEncoder.encode(registerInput.password()));
+
+        Set<ERole> roles = new HashSet<>();
+        roles.add(ERole.ROLE_RESIDENT);
+        user.setRoles(roles);
+
+        userRepository.save(user);
+    }
+}
