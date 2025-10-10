0fc55154a81b6be342618f2323b740d65cc8668f
montelzek <patrykmatras378@gmail.com>
2025-10-10 15:13:10 +0200

    feat: Add UserDetailsService implementation for authentication

---
+++ backend/src/main/java/com/montelzek/mydorm/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.montelzek.mydorm.security;
+
+import com.montelzek.mydorm.user.User;
+import com.montelzek.mydorm.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return UserDetailsImpl.build(user);
+    }
+}
