843ea9c60ef5c62ddd49aafec97a47c79549842e
montelzek <patrykmatras378@gmail.com>
2025-10-13 23:38:53 +0200

    feat: Upgrade Register UI to PrimeNG; implement dark mode toggle

--- frontend/src/app/auth/register/register.ts
+++ frontend/src/app/auth/register/register.ts
@@ -1,17 +1,27 @@
-import {Component, inject} from '@angular/core';
-import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Component, inject, OnInit} from '@angular/core';
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {Auth} from '../auth';
 import {Router, RouterLink} from '@angular/router';
 import {RegisterInput} from '../../graphql.types';
+import {Button, ButtonDirective} from 'primeng/button';
+import {Card} from 'primeng/card';
+import {FloatLabel} from 'primeng/floatlabel';
+import {InputText} from 'primeng/inputtext';
+import {Message} from 'primeng/message';
+import {Password} from 'primeng/password';
+import {ToggleSwitch} from 'primeng/toggleswitch';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, Button, ButtonDirective, Card, FloatLabel, InputText, Message, Password, ToggleSwitch, FormsModule, NgClass],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
-export class Register {
+export class Register implements OnInit {
 
+  errorMessage: string | null = null;
+  checked = false;
   private fb = inject(FormBuilder);
   private authService = inject(Auth);
   private router = inject(Router);
@@ -22,7 +32,6 @@ export class Register {
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]]
   });
-  errorMessage: string | null = null;
 
   onSubmit(): void {
     if (this.registerForm.invalid) {
@@ -36,5 +45,20 @@ export class Register {
         next: () => this.router.navigate(['/login']),
         error: (err) => this.errorMessage = err.message
       });
+  }
+
+  ngOnInit(): void {
+    this.checked = document.documentElement.classList.contains('my-app-dark');
+  }
+
+  toggleDarkMode(): void {
+    const html = document.documentElement;
+    if (this.checked) {
+      html.classList.add('my-app-dark');
+      localStorage.setItem('darkMode', 'true');
+    } else {
+      html.classList.remove('my-app-dark');
+      localStorage.setItem('darkMode', 'false');
+    }
   }
 }
