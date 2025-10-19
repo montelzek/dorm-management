33f38c365077bd2b4ba60e3786ba05d3714a0ea9
montelzek <patrykmatras378@gmail.com>
2025-10-20 01:05:02 +0200

    feat: Implement SidebarComponent for application navigation

--- frontend/src/app/shared/components/layout/sidebar/sidebar.ts
+++ frontend/src/app/shared/components/layout/sidebar/sidebar.ts
@@ -23,79 +23,78 @@ interface NavLink {
 export class SidebarComponent {
 
   private readonly authService = inject(AuthService);
-  
-  // Mobile menu state
+
   private readonly _isMobileMenuOpen = signal(false);
   readonly isMobileMenuOpen = this._isMobileMenuOpen.asReadonly();
 
   public navigationLinks: NavLink[] = [
-    { 
-      label: 'Dashboard', 
-      route: '/dashboard', 
-      iconPath: 'M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z' 
+    {
+      label: 'Dashboard',
+      route: '/dashboard',
+      iconPath: 'M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z'
     },
-    { 
-      label: 'Rezerwacje', 
-      route: '/reservation', 
+    {
+      label: 'Rezerwacje',
+      route: '/reservation',
       iconPath: 'M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0h18M8.25 9.75h7.5m-7.5 3h7.5m-7.5 3h7.5'
     },
-    { 
-      label: 'Usterki', 
-      route: '/issues', 
+    {
+      label: 'Usterki',
+      route: '/issues',
       iconPath: 'M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z',
       disabled: true
     },
-    { 
-      label: 'Przedłużanie pobytu', 
-      route: '/stay-extension', 
+    {
+      label: 'Przedłużanie pobytu',
+      route: '/stay-extension',
       iconPath: 'M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z',
       disabled: true
     },
-    { 
-      label: 'Kalendarz wydarzeń', 
-      route: '/events', 
+    {
+      label: 'Kalendarz wydarzeń',
+      route: '/events',
       iconPath: 'M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0h18',
       disabled: true
     },
-    { 
-      label: 'Tablica ogłoszeń', 
-      route: '/announcements', 
+    {
+      label: 'Tablica ogłoszeń',
+      route: '/announcements',
       iconPath: 'M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z',
       disabled: true
     },
-    { 
-      label: 'Marketplace', 
-      route: '/marketplace', 
+    {
+      label: 'Marketplace',
+      route: '/marketplace',
       iconPath: 'M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z',
       disabled: true
     },
-    { 
-      label: 'Centrum finansowe', 
-      route: '/finances', 
+    {
+      label: 'Centrum finansowe',
+      route: '/finances',
       iconPath: 'M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z',
       disabled: true
     },
-    { 
-      label: 'Paczki', 
-      route: '/packages', 
+    {
+      label: 'Paczki',
+      route: '/packages',
       iconPath: 'M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z',
       disabled: true
     },
-    { 
-      label: 'Rejestracja gości', 
-      route: '/guests', 
+    {
+      label: 'Rejestracja gości',
+      route: '/guests',
       iconPath: 'M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z',
       disabled: true
     },
-    { 
-      label: 'Baza wiedzy', 
-      route: '/knowledge-base', 
+    {
+      label: 'Baza wiedzy',
+      route: '/knowledge-base',
       iconPath: 'M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25',
       disabled: true
     },
-    { 
-      label: 'Profil', 
-      route: '/profile', 
+    {
+      label: 'Profil',
+      route: '/profile',
       iconPath: 'M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z',
       disabled: true
     }
@@ -111,7 +110,6 @@ export class SidebarComponent {
     this.authService.logout();
   }
 
-  // Mobile menu methods
   openMobileMenu(): void {
     this._isMobileMenuOpen.set(true);
   }
