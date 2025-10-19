96d798b436e6cfc820ae2ebbe92c61ca0988951c
montelzek <patrykmatras378@gmail.com>
2025-10-19 23:22:49 +0200

    Added interceptor to config

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,30 +1,35 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HttpHeaders } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import {Apollo, APOLLO_OPTIONS} from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { InMemoryCache, ApolloClientOptions } from '@apollo/client/core';
 import { setContext } from '@apollo/client/link/context';
+import { createHttpLink } from '@apollo/client/link/http';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
 import { routes } from './app.routes';
-import {providePrimeNG} from 'primeng/config';
-import {MyPreset} from './mypreset';
 
 const uri = 'http://localhost:8080/graphql';
 
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   const auth = setContext((_operation, _context) => {
     const token = localStorage.getItem('auth_token');
-    if (!token) {
-      return {};
+
+    if (token) {
+      return {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      };
     }
 
-    return { headers: new HttpHeaders().set('Authorization', `Bearer ${token}`) };
+    return {};
   });
 
-  const http = httpLink.create({ uri });
+  const http = createHttpLink({
+    uri: uri
+  });
 
   return {
     link: auth.concat(http),
@@ -34,17 +39,8 @@ export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: MyPreset,
-        options: {
-          darkModeSelector: '.my-app-dark'
-        }
-      }
-    }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([ErrorInterceptor])),
     Apollo,
     {
       provide: APOLLO_OPTIONS,
