99c7d18619576c8ffbd8acb86b5dc193da3c79c9
montelzek <patrykmatras378@gmail.com>
2025-10-20 00:54:20 +0200

    feat: Add all GraphQL queries for reservation and facility management

---
+++ frontend/src/app/features/resident/reservations/reservations.graphql.ts
@@ -0,0 +1,81 @@
+import { gql } from 'apollo-angular';
+
+export const GET_MY_DETAILS = gql`
+  query GetMyDetails {
+    me {
+      id
+      firstName
+      lastName
+      building {
+        id
+        name
+      }
+      room {
+        id
+        roomNumber
+      }
+    }
+  }
+`;
+
+export const GET_BUILDINGS = gql`
+  query GetBuildings {
+    allBuildings {
+      id
+      name
+    }
+  }
+`;
+
+export const GET_RESOURCES_BY_BUILDING = gql`
+  query GetResourcesByBuilding($buildingId: ID!) {
+    resourcesByBuilding(buildingId: $buildingId) {
+      id
+      name
+      resourceType
+    }
+  }
+`;
+
+export const GET_AVAILABLE_LAUNDRY_SLOTS = gql`
+  query GetAvailableLaundrySlots($resourceId: ID!, $date: String!) {
+    availableLaundrySlots(resourceId: $resourceId, date: $date) {
+      startTime
+      endTime
+    }
+  }
+`;
+
+export const GET_AVAILABLE_STANDARD_SLOTS = gql`
+  query GetAvailableStandardSlots($resourceId: ID!, $date: String!) {
+    availableStandardSlots(resourceId: $resourceId, date: $date) {
+      startTime
+      endTime
+    }
+  }
+`;
+
+export const GET_MY_RESERVATIONS = gql`
+  query GetMyReservations {
+    myReservations {
+      id
+      startTime
+      endTime
+      status
+      resource {
+        id
+        name
+      }
+    }
+  }
+`;
+
+export const CREATE_RESERVATION = gql`
+  mutation CreateReservation($input: CreateReservationInput!) {
+    createReservation(input: $input) {
+      id
+      startTime
+      endTime
+    }
+  }
+`;
