c92343e6a88d95af1dcbf0a11bfe567b35e1ce5e
montelzek <patrykmatras378@gmail.com>
2025-10-19 23:15:37 +0200

    Added imports

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GlobalErrorComponent } from './shared/components/ui/global-error/global-error';
+import { ToastComponent } from './shared/components/ui/toast/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GlobalErrorComponent, ToastComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class AppComponent {
   protected readonly title = signal('frontend');
 }
